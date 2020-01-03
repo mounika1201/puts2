25716879a752432261eae59a14e3eea921ec6ac3
mounika1201 <59233150+mounika1201@users.noreply.github.com>
2020-01-03 20:57:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,32 @@ def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+app.route('/send', methods=['POST'])
+def send(sum=sum):
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+
+        if operation == 'add':
+            sum = float(num1) + float(num2)
+            return render_template('app.html', sum=sum)
+
+        elif operation == 'subtract':
+            sum = float(num1) - float(num2)
+            return render_template('app.html', sum=sum)
+
+        elif operation == 'multiply':
+            sum = float(num1) * float(num2)
+            return render_template('app.html', sum=sum)
+
+        elif operation == 'divide':
+            sum = float(num1) / float(num2)
+            return render_template('app.html', sum=sum)
+        else:
+            return render_template('app.html')
 
 
-if __name__ == "__main__":
+if __name__ == ' __main__':
+    app.debug = True
     app.run()
