d91fb59919972f9aa1ad6bf3d5b4629270cf17c7
mounika1201 <mpsetti987@gmail.com>
2020-01-04 00:29:32 +0100

    modified

---
+++ add.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('app.html')
+
+
+@app.route('/send', methods=['POST'])
+def send(sum=sum):
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+
+        if operation == 'add':
+            sum = float(num1) + float(num2)
+            return render_template('app.html', sum=sum)
+if __name__ == ' __main__':
+    app.debug = True
+    app.run()
