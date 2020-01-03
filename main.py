144e2bc62c6e22bd29f8727342e4e033f0d7eec0
mounika1201 <59233150+mounika1201@users.noreply.github.com>
2020-01-03 22:30:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,37 +2,4 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
-
-app.route('/send', methods=['POST'])
-def send(sum=sum):
-    if request.method == 'POST':
-        num1 = request.form['num1']
-        num2 = request.form['num2']
-        operation = request.form['operation']
-
-        if operation == 'add':
-            sum = float(num1) + float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'subtract':
-            sum = float(num1) - float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'multiply':
-            sum = float(num1) * float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'divide':
-            sum = float(num1) / float(num2)
-            return render_template('app.html', sum=sum)
-        else:
-            return render_template('app.html')
-
-
-if __name__ == ' __main__':
-    app.debug = True
-    app.run()
