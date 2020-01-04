e1346a1961518c98a435cdea5f063d2932fec856
mounika1201 <mpsetti987@gmail.com>
2020-01-04 14:03:53 +0100

    modified

--- add.py
+++ add.py
@@ -11,17 +11,12 @@ def index():
         operation = request.form['operation']
     return 'Usage;\n<Operation>?A=<V1>&B=<V2>\n'
 
+
 @app.route('/add')
 def addition():
-    try:
-        num1, num2 = take_inputs()
-        result = num1 + num2
-    except ValueError:
-        warning_msg = take_inputs()
-        return warning_msg
-    else:
-        if float(result).is_integer():
-            result = int(result)
+    if operation == 'add':
+            sum = float(num1) + float(num2)
+    
             return '%d \n' % result
         return '%.2f \n' % result
 
