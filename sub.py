4162d71474cd0b5d222d16717e2e028768d65281
mounika1201 <mpsetti987@gmail.com>
2020-01-04 14:06:22 +0100

    modified

--- sub.py
+++ sub.py
@@ -1,4 +1,4 @@
- from flask import Flask, request
+from flask import Flask, request
 from fractions import Fraction
 
 app = Flask(__name__)
@@ -10,18 +10,13 @@ def index():
         num2 = request.form['num2']
         operation = request.form['operation']
     return 'Usage;\n<Operation>?A=<V1>&B=<V2>\n'
+
+
+@app.route('/add')
+def addition():
+    elif operation == 'subtract':
+            sum = float(num1) - float(num2)
     
-@app.route('/sub')
-def substraction():
-    try:
-        num1, num2 = take_inputs()
-        result = num1 - num2
-    except ValueError:
-        warning_msg = take_inputs()
-        return warning_msg
-    else:
-        if float(result).is_integer():
-            result = int(result)
             return '%d \n' % result
         return '%.2f \n' % result
 
