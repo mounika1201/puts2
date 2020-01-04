d9842cb611a5fe15ec1098aa241c5cf19caab8de
mounika1201 <mpsetti987@gmail.com>
2020-01-04 13:27:44 +0100

    additional changes

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ class Test(unittest.TestCase):
         def setUp(self):
             main.app.testing = True
             self.app = main.app.test_client()
-                 def test_addint(self):
+        def test_addint(self):
             rv =  self.app.get('/add?A=7&B=1')
             self.assertEqual(b'8.0', rv.result)
             self.assertNotEqual(b'6.000',rv.result)
         def test_addfloat(self):
             rv =  self.app.get('/add?A=6.7&B=2.2')
             self.assertEqual(b'8.9', rv.result)
-                self.app = main.app.test_client()
+            self.app = main.app.test_client()
         def test_subint(self):
             rv =  self.app.get('/sub?A=5&B=3')
             self.assertEqual(b'2.0', rv.result)
@@ -28,7 +28,7 @@ class Test(unittest.TestCase):
         def test_mulfloat(self):
             rv =  self.app.get('/mul?A=2.0&B=3.4')
             self.assertEqual(b'6.8', rv.result)
-        test_divint(self):
+        def test_divint(self):
             rv =  self.app.get('/div?A=10&B=5')
             self.assertEqual(b'2', rv.result)
             self.assertNotEqual(b'0.0256',rv.r̥esult)
