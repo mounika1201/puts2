5d97574432060ca7a6cc12e43b67b15d12e96a7e
mounika1201 <mpsetti987@gmail.com>
2020-01-04 13:52:53 +0100

    modified

--- sub.py
+++ sub.py
@@ -1,27 +1,29 @@
-  from flask import Flask, render_template, request
+ from flask import Flask, request
+from fractions import Fraction
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def main():
-    return render_template('app.html')
-
-
-@app.route('/send', methods=['POST'])
-def send(sum=sum):
+def index():
     if request.method == 'POST':
         num1 = request.form['num1']
         num2 = request.form['num2']
         operation = request.form['operation']
+    return 'Usage;\n<Operation>?A=<V1>&B=<V2>\n'
+    
+@app.route('/sub')
+def substraction():
+    try:
+        num1, num2 = take_inputs()
+        result = num1 - num2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
 
-        elif operation == 'subtract':
-            sum = float(num1) - float(num2)
-            return render_template('app.html', sum=sum)
-
-
-
-
-if __name__ == ' __main__':
-    app.debug = True
+if __name__ == "__main__":
     app.run()
