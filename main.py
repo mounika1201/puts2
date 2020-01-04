0750b3a467cdd2b68c713cf4d8afe2ef60884b7d
mounika1201 <mpsetti987@gmail.com>
2020-01-04 13:17:21 +0100

    modified

--- main.py
+++ main.py
@@ -1,41 +1,76 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
+from fractions import Fraction
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def main():
-    return render_template('app.html')
-
-
-@app.route('/send', methods=['POST'])
-def send(sum=sum):
+def index():
     if request.method == 'POST':
         num1 = request.form['num1']
         num2 = request.form['num2']
         operation = request.form['operation']
-
-        if operation == 'add':
-            sum = float(num1) + float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'subtract':
-            sum = float(num1) - float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'multiply':
-            sum = float(num1) * float(num2)
-            return render_template('app.html', sum=sum)
-
-        elif operation == 'divide':
-            sum = float(num1) / float(num2)
-            return render_template('app.html', sum=sum)
-        else:
-            return render_template('app.html')
+    return 'Usage;\n<Operation>?A=<V1>&B=<V2>\n'
 
 
-if __name__ == ' __main__':
-    app.debug = True
+@app.route('/sub')
+def substraction():
+    try:
+        num1, num2 = take_inputs()
+        result = num1 - num2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
+
+
+@app.route('/add')
+def addition():
+    try:
+        num1, num2 = take_inputs()
+        result = num1 + num2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
+
+
+@app.route('/mul')
+def multiplication():
+    try:
+        num1, num2 = take_inputs()
+        result = num1 * num2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
+
+
+@app.route('/div')
+def division():
+    try:
+        num1, num2 = take_inputs()
+        result = num1 / num2
+    except ValueError:
+        warning_msg = take_inputs()
+        return warning_msg
+    else:
+        if float(result).is_integer():
+            result = int(result)
+            return '%d \n' % result
+        return '%.2f \n' % result
+
+
+if __name__ == "__main__":
     app.run()
-    
-
